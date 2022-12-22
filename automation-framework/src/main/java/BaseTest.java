fdfa21ff49b23bfb2c5e3490581e9ab3b78edf90
samuhamm <samuhamm@sfu.ca>
2022-12-22 14:55:32 +0700

    Setup automation framework

---
+++ automation-framework/src/main/java/BaseTest.java
@@ -0,0 +1,38 @@
+import io.github.bonigarcia.wdm.managers.ChromeDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+public class BaseTest {
+
+    private WebDriver driver;
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.setProperty("headless", "false");
+        String headless = System.getProperty("headless");
+
+        ChromeDriverManager.chromedriver();
+        if("true".equals(headless)) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless");
+            driver = new ChromeDriver(chromeOptions);
+        } else {
+            driver = new ChromeDriver();
+        }
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        if (driver != null) {
+            driver.close();
+            driver.quit();
+        }
+    }
+
+    public WebDriver getDriver() {
+        return driver;
+    }
+}
---
+++ automation-page-objects/src/main/java/pages/loginPage.java
@@ -0,0 +1,4 @@
+package pages;
+
+public class loginPage extends BasePage {
+}
