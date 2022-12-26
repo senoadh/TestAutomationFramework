191a459ee7d856bfd564a1d3156747ac402edc91
samuhamm <samuhamm@sfu.ca>
2022-12-26 17:51:53 +0700

    Add login to homepage script

--- automation-tests/src/main/java/testCases/userHomePageTest.java
+++ automation-tests/src/main/java/testCases/userHomePageTest.java
@@ -3,13 +3,16 @@ package testCases;
 import base.BaseTest;
 import org.testng.annotations.Test;
 import pages.HomePage;
+import pages.LoginPage;
 
 public class userHomePageTest extends BaseTest {
 
     @Test
     public void validHomePageTest(){
-            HomePage homePage = new HomePage(getDriver());
-            homePage.verifyHomePageUrl();
-            homePage.verifyProductList();
+        LoginPage loginPage = new LoginPage(getDriver());
+        loginPage.loginToSauceDemo();
+        HomePage homePage = new HomePage(getDriver());
+        homePage.verifyHomePageUrl();
+        homePage.verifyProductList();
     }
 }
